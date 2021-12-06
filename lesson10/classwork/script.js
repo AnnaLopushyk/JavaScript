6e29150b3f08b66f5b44e40253eaeba77d071441
AnnaLopushyk <anna.lopushyk@gmail.com>
2021-12-06 13:47:25 +0200

    classwork

--- lesson10/classwork/script.js
+++ lesson10/classwork/script.js
@@ -3,8 +3,54 @@
 // та виводиться на консоль інформація з цих 2х форм.
 //     Кнопка повинна лежати за межами форм (Щоб ьуникнути  перезавантаження сторінки)
 // Доступ до інпутів через Forms API. Отже дайте формі та інпутам всі необхідні атрибути.
-//
-//
+
+let divOne = document.createElement('div');
+divOne.innerText='container 1';
+document.body.appendChild(divOne);
+let divTwo = document.createElement('div');
+divTwo.innerText = 'container 2';
+document.body.appendChild(divTwo);
+
+let formOne = document.createElement('form');
+formOne.setAttribute('name','formOne');
+divOne.appendChild(formOne);
+
+
+let formTwo = document.createElement('form');
+formTwo.setAttribute('name','formTwo');
+divTwo.appendChild(formTwo);
+
+let inputeOne = document.createElement('input');
+inputeOne.setAttribute('name','inputeOne');
+
+let inputeTwo = document.createElement('input');
+inputeTwo.setAttribute('name','inputeTwo');
+
+
+let inputeThree = document.createElement('input');
+inputeOne.setAttribute('name','inputeThree');
+
+let inputeFour = document.createElement('input');
+inputeTwo.setAttribute('name','inputeFour')
+
+
+let button = document.createElement('button');
+button.innerText = 'infoFromForms';
+button.style.marginTop='20px'
+document.body.appendChild(button);
+
+formOne.append(inputeOne,inputeTwo);
+formTwo.append(inputeThree,inputeFour);
+
+
+button.addEventListener('click', function (){
+    console.log(document.form.formOne.inputeOne.value);
+    console.log(document.form.formOne.inputeTwo.value);
+    console.log(document.form.formTwo.inputeThree.value);
+    console.log(document.form.formTwo.inputeFour.value);
+})
+
+
 // - Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
 //     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
 // (Додатковачастина для завдання)
